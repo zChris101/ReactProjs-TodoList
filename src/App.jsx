43d7c7b6ca5973658c91ec42921f073c949909f2
ChristianMusico <chrstnmusico@gmail.com>
2024-11-17 20:54:10 +0800

    Initial Code

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { useState ,useEffect } from "react";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [todoValue, setTodoValue] = useState("");
+
+  function persistData(newList) {
+    localStorage.setItem('todos',JSON.stringify({todos: newList}));
+  }
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo];
+    persistData(newTodoList);
+    setTodos(newTodoList);
+  }
+
+  function handleDeleteTodo(index) {
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex !== index;
+    });
+    persistData(newTodoList);
+
+    setTodos(newTodoList);
+  }
+
+  function handleEditTodo(index) {
+    const valueToBeEdited = todos[index];
+    setTodoValue(valueToBeEdited);
+    handleDeleteTodo(index);
+  }
+
+  useEffect(()=>{
+    if (!localStorage){
+      return
+    }
+
+    let localTodos = localStorage.getItem('todos')
+    if (!localTodos) { 
+      return 
+    }
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+
+  },[])
+
+  return (
+    <>
+      <TodoInput
+        handleAddTodos={handleAddTodos}
+        todoValue={todoValue}
+        setTodoValue={setTodoValue}
+      />
+      <TodoList
+        todos={todos}
+        handleDeleteTodo={handleDeleteTodo}
+        handleEditTodo={handleEditTodo}
+      />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+export default function TodoInput(props) {
+  const { handleAddTodos, todoValue, setTodoValue } = props;
+  
+
+  return (
+    <header>
+      <input
+        value={todoValue}
+        onChange={(e) => {
+          setTodoValue(e.target.value);
+        }}
+        placeholder="Enter a Todo . . ."
+      />
+      <button
+        onClick={() => {
+          handleAddTodos(todoValue);
+          setTodoValue('')
+        }}
+      >
+        Add
+      </button>
+    </header>
+  );
+}
